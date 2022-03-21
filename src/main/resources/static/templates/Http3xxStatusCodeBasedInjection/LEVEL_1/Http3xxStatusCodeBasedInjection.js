47565796f50bf76f9687875683c0977fde07ce35
Shailesh Jathar <shailesh.jathar@flexmoney.in>
2022-03-21 21:51:26 +0530

    10 commit

--- src/main/resources/static/templates/Http3xxStatusCodeBasedInjection/LEVEL_1/Http3xxStatusCodeBasedInjection.js
+++
@@ -1,8 +0,0 @@
-function updatePlaceholderDiv() {
-  let placeholderAnchorElement = document.getElementById("placeholder");
-  placeholderAnchorElement.href =
-    getUrlForVulnerabilityLevel() + "?returnTo=/VulnerableApp";
-  placeholderAnchorElement.innerText = "Click here";
-}
-
-updatePlaceholderDiv();
